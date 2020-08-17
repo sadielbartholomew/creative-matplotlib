15be8ed094302a008c78fc3035c7afe6fbf26ea0
Sadie Louise Bartholomew <sadie.bartholomew@ncas.ac.uk>
2020-08-17 03:01:48 +0100

    Julio Le Parc: populate dir & start to display outputs

---
+++ julio-le-parc-replications/README.md
@@ -0,0 +1,28 @@
+# Julio Le Parc Replications
+
+
+### Replication of selected artworks by Julio Le Parc
+
+
+#### Created by Sadie Bartholomew, 2020+
+
+Julio Le Parc (1928+)
+[is an Argentinian artist](http://www.julioleparc.org/bio.html).
+
+This mini-project is under development, but interim outputs will be shown
+below.
+
+
+## Interim Outputs
+
+#### Mutation of Forms
+
+Reconstruction of Le Parc's Mutation of Forms (1959), described for example
+on the
+[site of the The Metropolitan Museum of Art](https://www.metmuseum.org/art/collction/search/815337).
+
+*Note*: this is unfinished, namely the inner pairs of wedges need adjusting
+for angular extent and alignment, though the outermost wedge pairs are
+correct in that respect.
+
+![Mutation of Forms Replication](img/mutation_of_forms.png)
--- julio-le-parc-replications/master_script.py
+++ julio-le-parc-replications/master_script.py
@@ -1,15 +1,16 @@
 import itertools
 
-import numpy as np
 import matplotlib.path as mpath
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 
+import numpy as np
+
 
 COLOURS = {
     "MUTATION OF FORMS": {
-        "OFF WHITE": "#F8F0DB",
-        "RED": "#C8082D",
+        "OFF WHITE": "#FAEFDD",
+        "RED": "#CB0B22",
         "BLUE": "#1D119B",
     }
 }
@@ -21,15 +22,59 @@ number_points_per_side = 10
 padding_per_side = 2
 pad_points = 2
 
-fig, ax = plt.subplots(figsize=(12, 12))
+
+def create_angles_array(return_red=False, return_blue=False):
+    """ TODO. """
+    # NOTE: angles start pointing downwards i.e. 0 degs is south in PyPlot.
+    # So red wedges are constrained to -135 to +45, blues to +45 to +225.
+
+    # Original design angular coverage pattern is:
+    #
+    #     max blue ............. min blue
+    #     min red  .............  max red
+    #     ..    .                .     ..
+    #     ..      .            .       ..
+    #     ..        half blue          ..
+    #     ..         half red          ..
+    #     ..      .           .        ..
+    #     ..    .               .      ..
+    #     min blue ............. max blue
+    #     max red  .............  min red
+
+    # Define max and min angular coverages for the wedges:
+    # * red wedges go from -135 <- -45 -> +45
+    max_coverage_red = (-135, 45)
+    min_coverage_red = -45
+    # * blue wedges go from +45 <- +135 -> +225
+    max_coverage_blue = (45, 225)
+    min_coverage_blue = 135
+
+    # Use linspace to get 1D arrays of angles evenly spaced across coverage
+    red_theta1_min_to_max = np.linspace(
+        max_coverage_red[0], min_coverage_red, num=number_points_per_side)
+    red_theta2_min_to_max = np.linspace(
+        max_coverage_red[1], min_coverage_red, num=number_points_per_side)
+    blue_theta1_min_to_max = np.linspace(
+        max_coverage_blue[0], min_coverage_blue, num=number_points_per_side)
+    blue_theta2_min_to_max = np.linspace(
+        max_coverage_blue[1], min_coverage_blue, num=number_points_per_side)
+
+    # Zip up theta{1, 2} into an iterator to provide list of angle tuples later
+    red_linspace_thetas = zip(red_theta1_min_to_max, red_theta2_min_to_max)
+    blue_linspace_thetas = zip(blue_theta1_min_to_max, blue_theta2_min_to_max)
+
+    if return_red:
+        return list(red_linspace_thetas)
+    elif return_blue:
+        return list(blue_linspace_thetas)
 
 
-def plot_wedges(position):
+def plot_wedges(position, red_wedge_thetas, blue_wedge_thetas):
     """ TODO. """
     red_wedge = plot_wedge(
-        position, 270, 360, COLOURS["MUTATION OF FORMS"]["RED"])
+        position, *red_wedge_thetas, COLOURS["MUTATION OF FORMS"]["RED"])
     blue_wedge = plot_wedge(
-        position, 90, 180, COLOURS["MUTATION OF FORMS"]["BLUE"])
+        position, *blue_wedge_thetas, COLOURS["MUTATION OF FORMS"]["BLUE"])
     return red_wedge, blue_wedge
 
 
@@ -42,15 +87,45 @@ def plot_wedge(centre, theta1, theta2, colour):
     )
 
 
-grid_indices = range(number_points_per_side)
-for i, j in itertools.product(grid_indices, grid_indices):
-    position_xy = (pad_points + i, pad_points + j)
-    red_wedge, blue_wedge = plot_wedges(position_xy)
-    ax.add_patch(red_wedge)
-    ax.add_patch(blue_wedge)
+def plot_mutation_of_forms(axes):
+    """ TODO. """
+
+    grid_indices = range(number_points_per_side)
+    red_spaced_thetas = create_angles_array(return_red=True)
+    blue_spaced_thetas = create_angles_array(return_blue=True)
+    for i, j in itertools.product(grid_indices, grid_indices):
+        # Defaults for now while get the angles defined and in right places:
+        red_thetas = (-90, 0)
+        blue_thetas = (90, 180)
+
+        # Process just outer-most positioned wedges to check angle processing.
+        # TODO: consolidate this logic with the inner angle processing once
+        # that is in.
+        maximum_position = number_points_per_side - 1
+        if i == 0:
+            red_thetas = red_spaced_thetas[j]
+            blue_thetas = blue_spaced_thetas[::-1][j]
+        elif j == 0:
+            red_thetas = red_spaced_thetas[i]
+            blue_thetas = blue_spaced_thetas[::-1][i]
+        elif i == maximum_position:
+            red_thetas = red_spaced_thetas[::-1][j]
+            blue_thetas = blue_spaced_thetas[j]
+        elif j == maximum_position:
+            red_thetas = red_spaced_thetas[::-1][i]
+            blue_thetas = blue_spaced_thetas[i]
+
+        # Now create and plot the wedges onto the canvas:
+        position_xy = (pad_points + i, pad_points + j)
+        red_wedge, blue_wedge = plot_wedges(
+            position_xy, red_thetas, blue_thetas)
+        axes.add_patch(red_wedge)
+        axes.add_patch(blue_wedge)
 
 
 # Format canvas to centre on image and remove all axes and related markings
+fig, ax = plt.subplots(figsize=(6, 6))
+plot_mutation_of_forms(ax)
 fig.set_canvas(plt.gcf().canvas)
 fig.patch.set_facecolor(COLOURS["MUTATION OF FORMS"]["OFF WHITE"])
 limits = (
@@ -65,4 +140,6 @@ plt.xticks([])
 plt.yticks([])
 plt.tight_layout()
 
+# Plot, save and show. Done!
+plt.savefig('img/mutation_of_forms.png', format='png', bbox_inches='tight')
 plt.show()
