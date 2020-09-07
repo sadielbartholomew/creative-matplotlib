cfb50fae8c99cce148d7b7582d4ec4d25e8fc20c
Sadie Louise Bartholomew <sadie.bartholomew@ncas.ac.uk>
2020-09-07 02:15:31 +0100

    Julio Le Parc: tidy script, esp. to not re-calculate array in loop

--- julio-le-parc-replications/master_script.py
+++ julio-le-parc-replications/master_script.py
@@ -58,26 +58,28 @@ IMAGE_PAD_POINTS = 2
 
 def create_mutations_linspaced_angles(
         max_coverage, min_coverage, number_points_per_side):
-    """ TODO. """
-    # NOTE: angles start pointing downwards i.e. 0 degs is south in PyPlot.
-    # So red wedges are constrained to -135 to +45, blues to +45 to +225.
+    """ TODO.
 
-    # Original design angular coverage pattern is:
-    #
-    #     max blue ............. min blue
-    #     min red  .............  max red
-    #     ..    .                .     ..
-    #     ..      .            .       ..
-    #     ..        half blue          ..
-    #     ..         half red          ..
-    #     ..      .           .        ..
-    #     ..    .               .      ..
-    #     min blue ............. max blue
-    #     max red  .............  min red
+    NOTE: angles start pointing downwards i.e. 0 degs is south in PyPlot.
+    So red wedges are constrained to -135 to +45, blues to +45 to +225.
 
-    # Define max and min angular coverages for the wedges:
-    # * red wedges go from -135 <- -45 -> +45
-    # * blue wedges go from +45 <- +135 -> +225
+    Original design angular coverage pattern is:
+
+         max blue ............. min blue
+         min red  .............  max red
+         ..            ..             ..
+         ..            ..             ..
+         ...........half blue...........
+         ...........half red............
+         ..            ..             ..
+         ..            ..             ..
+         min blue ............. max blue
+         max red  .............  min red
+
+    Define max and min angular coverages for the wedges:
+    * red wedges go from -135 <- -45 -> +45
+    * blue wedges go from +45 <- +135 -> +225
+    """
 
     # Use linspace to get 1D arrays of angles evenly spaced across coverage:
     theta1_min_to_max = np.linspace(
@@ -91,21 +93,26 @@ def create_mutations_linspaced_angles(
 def create_mutation_angles_array(
         grid_indices, is_red=True, number_points_per_side=5):
     """ TODO. """
+    red_max = (-135, 45)
+    red_min = (-45, -45)
+    blue_max = (45, 225)
+    blue_min = (135, 135)
+
     angles_array = np.zeros(
         (number_points_per_side, number_points_per_side),
         dtype=(float, 2)
     )
 
     if is_red:
-        index = 1
+        index = 1  # don't change the spaced_thetas array later (c.f. -1)
         spaced_thetas = create_mutations_linspaced_angles(
-            max_coverage=(-135, 45), min_coverage=(-45, -45),
+            max_coverage=red_max, min_coverage=red_min,
             number_points_per_side=number_points_per_side,
         )
     else:
-        index = -1
+        index = -1  # to reverse the spaced_thetas array later, via [::-1]
         spaced_thetas = create_mutations_linspaced_angles(
-            max_coverage=(45, 225), min_coverage=(135, 135),
+            max_coverage=blue_max, min_coverage=blue_min,
             number_points_per_side=number_points_per_side,
         )
 
@@ -134,15 +141,17 @@ def create_rotations_angles_array(
     )
 
     spaced_thetas = np.linspace(0, 180, number_points_per_side)
-    # +/- signs are to achieve clockwise angle changes as per the design
     # 1. Make first and last column correct:
     for j in grid_indices:
         angles_array[0][j] = spaced_thetas[j]
         angles_array[-1][j] = spaced_thetas[-j-1]
     # 2. Create rows linearly-spaced based on first and last columns:
     for i in grid_indices:
+        # Minus sign is to achieve clockwise angle changes as per the design.
+        # Without it the angles would move from the first to the last angles
+        # in an anti-clockwise direction:
         row_angles = np.linspace(
-            -angles_array[0][i],
+            -1 * angles_array[0][i],  # see above regarding -1 factor
             angles_array[-1][i],
             number_points_per_side,
         )
@@ -162,7 +171,7 @@ def plot_mutations_wedges(
 def plot_mutations_wedge(centre, theta1, theta2, colour):
     """ TODO. """
     # 0.5 radius means the circles containing the wedges just touch their
-    # neighbours. Use 0.475 radius to provide a small gap as in the design.
+    # neighbours. Use 0.475 radius to provide a small gap as per the design.
     return mpatches.Wedge(
         centre, 0.475, theta1, theta2, color=colour
     )
@@ -223,15 +232,20 @@ def plot_mutation_of_forms(axes):
     design_name = "MUTATION OF FORMS"
     points = NUMBER_GRIDPOINTS_PER_SIDE[design_name]
     grid_points = range(points)
+
+    # Calculate angles:
+    red_angles_array = create_mutation_angles_array(
+        is_red=True, grid_indices=grid_points,
+        number_points_per_side=points
+    )
+    blue_angles_array = create_mutation_angles_array(
+        is_red=False, grid_indices=grid_points,
+        number_points_per_side=points
+    )
     for i, j in itertools.product(grid_points, grid_points):
-        red_thetas = create_mutation_angles_array(
-            is_red=True, grid_indices=grid_points,
-            number_points_per_side=points
-        )[i][j]
-        blue_thetas = create_mutation_angles_array(
-            is_red=False, grid_indices=grid_points,
-            number_points_per_side=points
-        )[i][j]
+        # Get angles:
+        red_thetas = red_angles_array[i][j]
+        blue_thetas = blue_angles_array[i][j]
 
         # Now create and plot the wedges onto the canvas:
         position_xy = (IMAGE_PAD_POINTS + i, IMAGE_PAD_POINTS + j)
@@ -249,6 +263,7 @@ def plot_rotations(axes):
     design_name = "ROTATIONS"
     points = NUMBER_GRIDPOINTS_PER_SIDE[design_name]
     grid_points = range(points)
+
     angles_array = create_rotations_angles_array(
         grid_points,
         number_points_per_side=points
