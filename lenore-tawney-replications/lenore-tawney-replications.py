6688322dece34a38efb85433640c73db1796cff0
Sadie Louise Bartholomew <sadie.bartholomew@ncas.ac.uk>
2021-07-02 22:18:42 +0100

    Lenore Tawney: recreate The Great Breath w/ multiple line pair joining

--- lenore-tawney-replications/lenore-tawney-replications.py
+++ lenore-tawney-replications/lenore-tawney-replications.py
@@ -14,8 +14,9 @@ from matplotlib.ticker import MultipleLocator
 
 BACKGROUND_COLOUR = "#FBF4EA"  # beige colour taken from photo of an original
 GRID_COLOUR = "powderblue"
-DEFAULT_LINE_COLOUR = "#1D1616"  # off-black, very dark grey for better effect
-LINEWIDTH = 0.5
+DEFAULT_LINE_COLOUR = "#221111"  # dark murky brown
+LINEWIDTH = 0.8
+LINE_ALPHA = 0.5
 
 FIGSIZE = (11, 6.75)  # like landscape A4 (graph-gridded) paper
 
@@ -29,7 +30,7 @@ else:
 
 
 def plot_line_segment(
-    startpoint_coors, endpoint_coors, colour=DEFAULT_LINE_COLOUR
+    startpoint_coors, endpoint_coors, colour=DEFAULT_LINE_COLOUR, alpha=1.0
 ):
     """TODO."""
     plt.plot(
@@ -37,6 +38,7 @@ def plot_line_segment(
         [startpoint_coors[1], endpoint_coors[1]],
         color=colour,
         linewidth=LINEWIDTH,
+        alpha=alpha,
     )
 
 
@@ -55,13 +57,14 @@ def draw_between_line_segments(
     line_seg_2,
     number_lines_to_draw=50,
     colour=DEFAULT_LINE_COLOUR,
+    alpha=1.0,
 ):
     """TODO."""
     xs = np.linspace(line_seg_1[0], line_seg_1[1], num=number_lines_to_draw)
     ys = np.linspace(line_seg_2[1], line_seg_2[0], num=number_lines_to_draw)
     for x, y in zip(xs, ys):
         print(x, y)
-        plot_line_segment(x, y, colour=colour)
+        plot_line_segment(x, y, colour=colour, alpha=alpha)
 
 
 def draw_from_point_to_line_segment(
@@ -69,11 +72,12 @@ def draw_from_point_to_line_segment(
     line_seg,
     number_lines_to_draw=50,
     colour=DEFAULT_LINE_COLOUR,
+    alpha=1.0,
 ):
     """TODO."""
     ys = np.linspace(line_seg[0], line_seg[1], num=number_lines_to_draw)
     for y in ys:
-        plot_line_segment(point, y, colour=colour)
+        plot_line_segment(point, y, colour=colour, alpha=alpha)
 
 
 def format_grids(ax):
@@ -145,28 +149,39 @@ def post_format_plot(ax, view_axes_labels_as_guide=False):
     plt.tight_layout()
 
 
-def plot_overall_design(line_coors, view_axes_labels_as_guide=False):
+def plot_overall_design(
+    line_coors, coor_pairs_to_join, view_axes_labels_as_guide=False
+):
     """TODO."""
     fig, ax = pre_format_plot()
 
     # Plot the lines comprising the design
     for line_coor in line_coors:
-        plot_line_segment(*line_coor)
+        plot_line_segment(*line_coor, alpha=LINE_ALPHA)
 
     # Test a drawing of equally-spaced lines between two segments
-    draw_between_line_segments(line_coors[0], line_coors[3], 75)
-    draw_between_line_segments(line_coors[1], line_coors[2], 75)
-    draw_from_point_to_line_segment((0, 0), line_coors[0], colour="indianred")
+    for pairs in coor_pairs_to_join:
+        index_1, index_y = pairs
+        draw_between_line_segments(
+            line_coors[index_1], line_coors[index_y], 60, alpha=LINE_ALPHA
+        )
 
     post_format_plot(ax, view_axes_labels_as_guide=view_axes_labels_as_guide)
     plt.show()
 
 
-# Plot
+# Plot to recreate LT's work 'The Great Breath, 1964' (see:
+# https://lenoretawney.org/work/the-great-breath/)
 line_coors = [
     ((9, 81), (80, 92)),  # 1, join to 4
-    ((80, 92), (150, 80)),  # 2, join to 3
-    ((9, 15), (80, 6)),  # 3, join to 2
+    ((80, 92), (150, 81)),  # 2, join to 3
+    ((10, 15), (80, 6)),  # 3, join to 2
     ((80, 6), (150, 15)),  # 4, join to 1
+    ((10, 40), (80, 6)),  # 5, NEW join to 2
+    ((80, 6), (150, 40)),  # 6, NEW join to 1
 ]
-plot_overall_design(line_coors, view_axes_labels_as_guide=True)
+# As with 'join' comments above, but minus 1 for zero-based indexing...
+coor_pairs_to_join = [(0, 3), (1, 2), (1, 4), (0, 5)]
+plot_overall_design(
+    line_coors, coor_pairs_to_join, view_axes_labels_as_guide=True
+)
